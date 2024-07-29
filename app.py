d3c93d2212ac93896e612331ee27f73b44b2f820
paradox974333 <159344083+paradox974333@users.noreply.github.com>
2024-07-29 22:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import threading
 import logging
 
 app = Flask(__name__)
+CORS(app)  # This enables CORS for all routes
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
